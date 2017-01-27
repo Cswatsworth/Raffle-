2fd2a0ab7d6b229525ad83e454b202b754054dc7
Chad Swatsworth <Chad Swatsworth>
2017-01-27 15:48:18 -0500

    empty array returns 0

--- test_raffle.rb
+++ test_raffle.rb
@@ -6,4 +6,9 @@ class TestRaffleArray < Minitest::Test
 	def test_create_empty_array
 		assert_equal(Array, [].class)
 	end
+
+	def test_empty_array_not_equal_1
+		assert_equal(false, Array == 1)
+	end
+
 end
